2768d11d1781544342f192c9dc35ecd17e8a6dd9
apad27 <103241178+apad27@users.noreply.github.com>
2024-08-07 23:55:58 +0700

    ja

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -11,7 +11,7 @@ const Footer: React.FunctionComponent<IFooterProps> = (props) => {
     <section className="bg-black pb-[5rem] pt-10 md:px-[9rem]">
       <div className="container flex flex-col justify-between align-middle text-white md:flex-row">
         <div className="flex flex-col">
-          <h1>PT. Jawa Agrindo Internasional @ 2024 </h1>
+          <h1>PT. Digital Laboratorium Indonesia </h1>
           <h1>All Rights Reserved.</h1>
         </div>
         <div className="flex gap-4 pt-5 text-2xl">
